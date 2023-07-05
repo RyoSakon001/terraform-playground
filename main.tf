88b385aafdd24d8af6eb5310d2b61ee58ef08758
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-05 12:17:41 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+
+provider "aws" {
+  profile = "terraform"
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "hello-terraform" {
+  ami = "ami-0cfc97bf81f2eadc4"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0cff88690a3299bc2"
+}
