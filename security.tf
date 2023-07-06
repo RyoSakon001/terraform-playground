7ec2604a0a36884e25782f4f7759d836368db92d
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-07 00:59:55 +0900

    Security Group App, OM, DB

--- security.tf
+++ security.tf
@@ -1,8 +1,8 @@
 # Web Security Group
 resource "aws_security_group" "web_sg" {
-  name = "${var.project}-${var.environment}-web-sg"
+  name        = "${var.project}-${var.environment}-web-sg"
   description = "Web Security Group"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.vpc.id
 
   tags = {
     Name        = "${var.project}-${var.environment}-web-sg"
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "web_in_http" {
   protocol          = "http"
   from_port         = 80
   to_port           = 80
-  cidr_block = "0.0.0.0/0"
+  cidr_block        = "0.0.0.0/0"
 }
 
 resource "aws_security_group_rule" "web_in_https" {
@@ -26,14 +26,99 @@ resource "aws_security_group_rule" "web_in_https" {
   protocol          = "https"
   from_port         = 443
   to_port           = 443
-  cidr_block = "0.0.0.0/0"
+  cidr_block        = "0.0.0.0/0"
 }
 
 resource "aws_security_group_rule" "web_out_tcp3000" {
-  security_group_id = aws_security_group.web_sg.id
-  type              = "egress"
+  security_group_id        = aws_security_group.web_sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 3000
+  to_port                  = 3000
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
+# App Security Group
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project}-${var.environment}-app-sg"
+  description = "App Security Group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-app-sg"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+# TODO: セキュリティグループルールを追加
+
+# Operation Management Security Group
+resource "aws_security_group" "om_sg" {
+  name        = "${var.project}-${var.environment}-om-sg"
+  description = "Operation Management Security Group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-om-sg"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "om_in_ssh" {
+  security_group_id = aws_security_group.om_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_block        = "0.0.0.0/0"
+}
+resource "aws_security_group_rule" "om_in_tcp3000" {
+  security_group_id = aws_security_group.om_sg.id
+  type              = "ingress"
   protocol          = "tcp"
   from_port         = 3000
   to_port           = 3000
-  cidr_block = "0.0.0.0/0"
+  cidr_block        = "0.0.0.0/0"
+}
+
+resource "aws_security_group_rule" "om_in_http" {
+  security_group_id = aws_security_group.om_sg.id
+  type              = "ingress"
+  protocol          = "http"
+  from_port         = 80
+  to_port           = 80
+  cidr_block        = "0.0.0.0/0"
+}
+
+resource "aws_security_group_rule" "om_in_https" {
+  security_group_id = aws_security_group.om_sg.id
+  type              = "ingress"
+  protocol          = "https"
+  from_port         = 443
+  to_port           = 443
+  cidr_block        = "0.0.0.0/0"
+}
+
+# DB Security Group
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.environment}-db-sg"
+  description = "DB Security Group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-db-sg"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "db_in_mysql" {
+  security_group_id = aws_security_group.db_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3306
+  to_port           = 3306
+  cidr_block        = "0.0.0.0/0"
 }
