1439124ba286c2768a0d19282f802f21534aa6d0
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-07 00:59:17 +0900

    Internet Gateway

--- network.tf
+++ network.tf
@@ -128,6 +128,6 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_route" "public_internet_access" {
   route_table_id         = aws_route_table.public_rt.id
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
