100ba539ff241fafbefe1c29295d9750c7b79f78
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-08 20:12:52 +0900

    EC2 App Server

--- appserver.tf
+++ appserver.tf
@@ -9,3 +9,26 @@ resource "aws_key_pair" "keypair" {
     Environment = var.environment
   }
 }
+
+# EC2 Instance
+resource "aws_instance" "app_server" {
+  # 基本情報
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  # セキュリティ
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id
+    , aws_security_group.om_sg.id
+
+  ]
+  key_name = aws_key_pair.keypair.key_name
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-app-ec2"
+    Project     = var.project
+    Environment = var.environment
+    Type        = "app"
+  }
+}
