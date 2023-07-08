78579856ac71800a338c23a9c460b7bc63a550f9
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-08 10:28:44 +0900

    Security Group App, OM, DB

--- security.tf
+++ security.tf
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "web_in_http" {
   protocol          = "tcp"
   from_port         = 80
   to_port           = 80
-  cidr_blocks        = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "web_in_https" {
@@ -26,7 +26,7 @@ resource "aws_security_group_rule" "web_in_https" {
   protocol          = "tcp"
   from_port         = 443
   to_port           = 443
-  cidr_blocks        = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "web_out_tcp3000" {
@@ -72,7 +72,7 @@ resource "aws_security_group_rule" "om_in_ssh" {
   protocol          = "tcp"
   from_port         = 22
   to_port           = 22
-  cidr_blocks        = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 resource "aws_security_group_rule" "om_in_tcp3000" {
   security_group_id = aws_security_group.om_sg.id
@@ -80,7 +80,7 @@ resource "aws_security_group_rule" "om_in_tcp3000" {
   protocol          = "tcp"
   from_port         = 3000
   to_port           = 3000
-  cidr_blocks        = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "om_out_http" {
@@ -89,7 +89,7 @@ resource "aws_security_group_rule" "om_out_http" {
   protocol          = "tcp"
   from_port         = 80
   to_port           = 80
-  cidr_blocks        = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "om_out_https" {
@@ -98,7 +98,7 @@ resource "aws_security_group_rule" "om_out_https" {
   protocol          = "tcp"
   from_port         = 443
   to_port           = 443
-  cidr_blocks        = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # DB Security Group
@@ -115,10 +115,10 @@ resource "aws_security_group" "db_sg" {
 }
 
 resource "aws_security_group_rule" "db_in_mysql" {
-  security_group_id = aws_security_group.db_sg.id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 3306
-  to_port           = 3306
+  security_group_id        = aws_security_group.db_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
   source_security_group_id = aws_security_group.app_sg.id
 }
