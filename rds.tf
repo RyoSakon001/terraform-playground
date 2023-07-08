51cc809dc595b953eb9a5d4a1a21456bc73d32bd
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-08 12:12:22 +0900

    DB Parameter, Option, Subnet Group

---
+++ rds.tf
@@ -0,0 +1,35 @@
+# Parameter Group
+resource "aws_db_parameter_group" "mysql_standalone_parameter_group" {
+  name   = "${var.project}-${var.environment}-mysql-standalone-parameter-group"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
+
+# Option Group
+resource "aws_db_option_group" "mysql_standalone_option_group" {
+  name                 = "${var.project}-${var.environment}-mysql-standalone-option-group"
+  engine_name          = "mysql"
+  major_engine_version = "8.0"
+}
+
+# Subnet Group
+resource "aws_db_subnet_group" "mysql_standalone_subnet_group" {
+  name = "${var.project}-${var.environment}-mysql-standalone-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet_1a.id,
+    aws_subnet.private_subnet_1c.id
+  ]
+  tags = {
+    Name        = "${var.project}-${var.environment}-mysql-standalone-subnet-group"
+    Project     = var.project
+    Environment = var.environment
+  }
+}
