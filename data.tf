858dcb9ece8a229de7a0cccce6c82fb345827f53
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-25 01:25:09 +0900

    launch template

--- data.tf
+++ data.tf
@@ -7,14 +7,18 @@ data "aws_ami" "app" {
   owners      = ["self", "amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+    values = ["terraform-playground-*-ami"]
   }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  # filter {
+  #   name   = "name"
+  #   values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  # }
+  # filter {
+  #   name   = "root-device-type"
+  #   values = ["ebs"]
+  # }
+  # filter {
+  #   name   = "virtualization-type"
+  #   values = ["hvm"]
+  # }
 }
