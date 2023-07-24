b12eb47ea492218b0c334a7e0b51e26b0750efb7
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-24 18:46:49 +0900

    https

--- elb.tf
+++ elb.tf
@@ -31,6 +31,18 @@ resource "aws_lb_target_group_attachment" "instance" {
 }
 
 # ALB Listener
+resource "aws_lb_listener" "alb_listener_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.tokyo_cert.arn
+
+  default_action {
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    type             = "forward"
+  }
+}
 resource "aws_lb_listener" "alb_listener_http" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
